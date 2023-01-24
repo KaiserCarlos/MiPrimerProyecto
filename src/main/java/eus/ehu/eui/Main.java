aa7f9cdcad496aae403b0d569819901b8b09a9a8
KaiserCarlos <carlos.vilarchao710@gmail.com>
2023-01-24 17:34:59 +0100

    Version definitiva

--- src/main/java/eus/ehu/eui/Main.java
+++ src/main/java/eus/ehu/eui/Main.java
@@ -3,6 +3,7 @@ package eus.ehu.eui;
 public class Main {
     public static void main(String[] args) {
 
+        System.out.println("Otro mensaje");
         System.out.println("Otra version");
     }
 }
